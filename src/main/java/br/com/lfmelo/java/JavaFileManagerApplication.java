4326cb2583fe21e7938f591aed193ba0ff1d30f1
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-03-29 11:54:16 -0300

    feat: start project

---
+++ src/main/java/br/com/lfmelo/java/JavaFileManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lfmelo.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lfmelo/java/JavaFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lfmelo.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
